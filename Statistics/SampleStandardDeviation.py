979fae8f4ffa8720bff782a8a76ac1351ab2c9f2
vishwanath7 <ezhilvishwanath@gmail.com>
2019-11-07 23:56:22 -0500

    Feature: Updated Sample Standard Deviation with calculator functions

--- Statistics/SampleStandardDeviation.py
+++ Statistics/SampleStandardDeviation.py
@@ -1,24 +1,29 @@
 import math
 from Calculator.Addition import addition
 from Calculator.Division import division
+from Calculator.Subtraction import subtraction
+from Calculator.Square import square
 from Calculator.Sqrt import root
 from Statistics.PopulationMean import mean
 
 def sampst_dev(lst):
     sum,new_sum = 0,0
     new_list = []
-    i,j = 0,0
 
     for x in range(5):
         new_list = lst[0:5]
-        new_sum+= new_list[x]
-        j += 1
-    new_mean = new_sum/j
+        new_sum = addition(new_sum, new_list[x])
+    j = len(new_list)
+    new_mean = division(j, new_sum)
+
     for row in new_list:
-        i=i+1
-        error = row - new_mean
-        error_sq = error*error
-        sum = sum + error_sq
-    b=sum/(i-1)
-    std = math.sqrt(b)
+        error = subtraction(row, new_mean)
+        error_sq = square(error)
+        sum = addition(sum, error_sq)
+    i = len(new_list)
+    b = division(subtraction(1,i), sum)
+    std = math.sqrt(b)  #works
+    #std = root(b)      #Doesn't work
     return std
+
+
