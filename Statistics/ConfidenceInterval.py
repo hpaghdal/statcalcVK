7b2a1db1dbb037c5e0158aeeff90cbe224f131fa
vishwanath7 <ezhilvishwanath@gmail.com>
2019-11-09 18:25:37 -0500

    fix: deleted text

--- Statistics/ConfidenceInterval.py
+++ Statistics/ConfidenceInterval.py
@@ -20,12 +20,10 @@ def confidenceinterval(lst, conf):
         t = 2.58
     else:  # 95 default confidence percentage
         t = 1.96
-    #std_error = std / (math.sqrt(len(lst)))
+
     std_error = division(std , math.sqrt(len(lst)))
-    #conf_upper = x + t * std_error
     conf_upper = addition(x , multiplication(t, std_error))
     conf_upper = round(conf_upper, 2)
-    #conf_lower = x - t * std_error
     conf_lower = subtraction(multiplication(t, std_error), x)
     conf_lower = round(conf_lower, 2)
     return conf_upper, conf_lower
