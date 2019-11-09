43aaa97e08c2189824b275da7be20edba28ddc45
vishwanath7 <ezhilvishwanath@gmail.com>
2019-11-09 13:28:22 -0500

    Feature: Added function and test for Sample Mean using CSV file data

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -14,18 +14,28 @@ class MyTestCase(unittest.TestCase):
     def test_decorator_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
 
-    def test_mean_calculator(self):
-        # test_data = CsvReader('/Tests/Data/UnitTestStatsAnswers.csv').data
-        # for row in test_data:
-        data = [1, 2, 3, 4, 5]
-        self.assertEqual(self.statistics.popmean(data), 3)
-        # self.assertEqual(self.calculator.popmean(row['Value 1']), 16.5)
-        # self.assertEqual(self.calculator.popmean(x), int(16.5))
 
-    def test_sample_mean_calculator(self):
-        data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.assertEqual(self.statistics.sammean(data), 3)
+    def test_Population_Mean_calculator(self):
+        test_data = CsvReader('Tests/Data/StatCalcData.csv').data
+        answer = CsvReader('Tests/Data/UnitTestStatsAnswers.csv').data
+        lst = []
+        for row in test_data:
+            y = int(row['Value 1'])
+            lst.append(y)
+        # for column in answer:
+        # result = float((column['mean']))
+        self.assertEqual(self.statistics.newmean(lst), 72.94494494494495)
 
+
+    def test_Sample_Mean_calculator(self):
+        test_data = CsvReader('Tests/Data/StatCalcData.csv').data
+        lst = []
+        for row in test_data:
+            y = int(row['Value 1'])
+            lst.append(y)
+        x, z = self.statistics.sammean(lst)
+
+        self.assertEqual(x, z)
 
     def test_median_calculator(self):
         ##test_data = CsvReader('/Tests/Data/Addition.csv').data
@@ -39,11 +49,11 @@ class MyTestCase(unittest.TestCase):
         data = [1, 2, 3, 4, 5, 6, 3]
         self.assertEqual(self.statistics.mod(data), 3)
 
-    def test_standard_deviation_calculator(self):
+    def test_Standard_Deviation_calculator(self):
         data = [1, 2, 3, 4, 5]
         self.assertEqual(self.statistics.stddev(data), 1.5811388300841898)
 
-    def test_sample_standard_deviation_calculator(self):
+    def test_Sample_Standard_Deviation_calculator(self):
 
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
         lst = []
@@ -53,8 +63,6 @@ class MyTestCase(unittest.TestCase):
         x, z = self.statistics.sampstdev(lst)
         x = round(x, 3)
         z = round(z, 3)
-        # z = statistics.stdev(lst)
-        # x = self.statistics.sampstdev()
 
         self.assertEqual(x, z)
 
@@ -72,16 +80,6 @@ class MyTestCase(unittest.TestCase):
         data = [1, 2, 3, 4, 5]
         self.assertEqual(self.statistics.pvariance(data), 2.5000000000000004)
 
-    def test_new_mean_statistics(self):
-        test_data = CsvReader('Tests/Data/StatCalcData.csv').data
-        answer = CsvReader('Tests/Data/UnitTestStatsAnswers.csv').data
-        lst = []
-        for row in test_data:
-            y = int(row['Value 1'])
-            lst.append(y)
-        # for column in answer:
-        # result = float((column['mean']))
-        self.assertEqual(self.statistics.newmean(lst), 72.94494494494495)
 
     def test_proportion_calculator(self):
         data = [1, 2, 3, 4, 5, 6]
