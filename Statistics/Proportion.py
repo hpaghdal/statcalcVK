4096e9a1673f923abb4c71e7b0325b4a912e90ef
Karan <kwalanj@gmail.com>
2019-11-10 18:27:49 -0500

    Fix: Proportion function formatting fix

--- Statistics/Proportion.py
+++ Statistics/Proportion.py
@@ -3,10 +3,11 @@ from Calculator.Multiplication import multiplication
 from Calculator.Division import division
 from Calculator.Addition import addition
 
+
 def proportion(lst):
     count = 0
     for i in lst:
-        if (i%2) == 0:
+        if (i % 2) == 0:
             count = addition(count, 1)
         prop = division(count, len(lst))
     return prop
