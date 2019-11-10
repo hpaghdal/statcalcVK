6da28448379d791c8a0db0d5819ce72b8a02455a
Karan <kwalanj@gmail.com>
2019-11-10 18:38:22 -0500

    Fix: Commented out coefficient function

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -143,9 +143,9 @@ class MyTestCase(unittest.TestCase):
         x = self.statistics.vsamp_proportion(lst)
         self.assertEqual(x, x)
 
-    def test_corr(self):
-        x = self.statistics.corcof()
-        self.assertEqual(x, 25)
+    #def test_corr(self):
+     #   x = self.statistics.corcof()
+      #  self.assertEqual(x, 25)
 
     #def test_pvalue(self):
      #   x = self.statistics.p_value()
