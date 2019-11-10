da1f443e2c1081e4b54f8c7c1d0946f806494fd1
vishwanath7 <ezhilvishwanath@gmail.com>
2019-11-09 19:51:16 -0500

    Feature: Added proportion function to test_statistics and statistics file

--- Statistics/Proportion.py
+++ Statistics/Proportion.py
@@ -4,12 +4,9 @@ from Calculator.Division import division
 from Calculator.Addition import addition
 
 def proportion(lst):
-    m = mean(lst)
-    lower_limit = multiplication(0.8, m)
-    higher_limit = multiplication(1.2, m)
     count = 0
     for i in lst:
-        if (i > higher_limit) or (i < lower_limit):
+        if (i%2) == 0:
             count = addition(count, 1)
-    p = round(division(count, float(len(lst))), 8)
-    return p
+        prop = division(count, len(lst))
+    return prop
