43cce3d9a92b0b1e5a3fdfbf92c45b6852d4b5a6
vishwanath7 <ezhilvishwanath@gmail.com>
2019-11-10 20:24:10 -0500

    Task: Updated test_statistics file with data_add function

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -23,13 +23,11 @@ class MyTestCase(unittest.TestCase):
         for column in answer:
             self.assertEqual(self.statistics.newmean(lst), float((column['mean'])))
 
-
     def test_Sample_Mean_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
         lst = data_add(test_data)
         x, z = self.statistics.sammean(lst)
         self.assertEqual(x, z)
-
 
     def test_Median_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
@@ -38,82 +36,54 @@ class MyTestCase(unittest.TestCase):
         for column in answer:
             self.assertEqual(self.statistics.med(lst), float((column['median'])))
 
-
     def test_Mode_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
         answer = CsvReader('Tests/Data/StatAnswers.csv').data
-        lst = []
-        for row in test_data:
-            y = int(row['Value 1'])
-            lst.append(y)
+        lst = data_add(test_data)
         for column in answer:
-            result = float((column['mode']))
-        self.assertEqual(self.statistics.mod(lst), result)
+            self.assertEqual(self.statistics.mod(lst), float((column['mode'])))
 
     def test_Population_Standard_Deviation_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
         answer = CsvReader('Tests/Data/StatAnswers.csv').data
-        lst = []
-        for row in test_data:
-            y = int(row['Value 1'])
-            lst.append(y)
+        lst = data_add(test_data)
         for column in answer:
-            result = float((column['stdev']))
-        self.assertEqual(self.statistics.stddev(lst), result)
+            self.assertEqual(self.statistics.stddev(lst), float((column['stdev'])))
 
     def test_Sample_Standard_Deviation_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
-        lst = []
-        for row in test_data:
-            y = int(row['Value 1'])
-            lst.append(y)
+        lst = data_add(test_data)
         x, z = self.statistics.sampstdev(lst)
         x = round(x, 3)
         z = round(z, 3)
-
         self.assertEqual(x, z)
 
     def test_confidence_interval_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
-        lst = []
-        for row in test_data:
-            y = int(row['Value 1'])
-            lst.append(y)
+        lst = data_add(test_data)
         conf = 95
         self.assertEqual(self.statistics.confintv(lst, conf), (132.67, 121.07))
 
     def test_zscore_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
         answer = CsvReader('Tests/Data/StatAnswers.csv').data
-        lst = []
-        for row in test_data:
-            y = int(row['Value 1'])
-            lst.append(y)
+        lst = data_add(test_data)
         for column in answer:
-            result = float((column['zscore']))
-        self.assertEqual(self.statistics.z_score(lst), result)
+            self.assertEqual(self.statistics.z_score(lst), float((column['zscore'])))
 
     def test_population_variance_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
         answer = CsvReader('Tests/Data/StatAnswers.csv').data
-        lst = []
-        for row in test_data:
-            y = int(row['Value 1'])
-            lst.append(y)
+        lst = data_add(test_data)
         for column in answer:
-            result = float((column['pop_variance']))
-        self.assertEqual(self.statistics.pvariance(lst), result)
+            self.assertEqual(self.statistics.pvariance(lst), float((column['pop_variance'])))
 
     def test_proportion_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
         answer = CsvReader('Tests/Data/StatAnswers.csv').data
-        lst = []
-        for row in test_data:
-            y = int(row['Value 1'])
-            lst.append(y)
+        lst = data_add(test_data)
         for column in answer:
-            result = float((column['proportion']))
-        self.assertEqual(self.statistics.proportion(lst), result)
+            self.assertEqual(self.statistics.proportion(lst), float((column['proportion'])))
 
     def test_variance_population_proportion_calculator(self):
         test_data = CsvReader('Tests/Data/StatCalcData.csv').data
@@ -144,9 +114,9 @@ class MyTestCase(unittest.TestCase):
         x = self.statistics.corcof(lst)
         self.assertEqual(x, x)
 
-    #def test_pvalue(self):
-     #   x = self.statistics.p_value()
-      #  self.assertEqual(x, 25)
+    # def test_pvalue(self):
+    #   x = self.statistics.p_value()
+    #  self.assertEqual(x, 25)
 
 
 if __name__ == '__main__':
